04aa4dd7e6ddd932bb0ebee803702032b66f696a
Juan Martin Gallo <jgallo@effectussoftware.com>
2017-11-23 16:58:40 -0300

    Routes fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import configureStore from './store';
 import { MANAGE_USER, MANAGE_RECORD, MANAGEMENT, DASHBOARD, LOGIN, HOME } from './consts/routes';
 
 import ManageUser from './containers/ManageUser';
-import EditRecord from './containers/EditRecord';
+import ManageRecord from './containers/ManageRecord';
 import Manager from './containers/Management';
 import Dashboard from './containers/Dashboard';
 import SignUp from './containers/SignUp';
@@ -23,8 +23,8 @@ render(
             <Switch>
                 <Route path={`${MANAGE_USER}/:uid`} component={ManageUser} />
                 <Route path={MANAGE_USER} component={ManageUser} />
-                <Route path={`${MANAGE_RECORD}/:recordId`} component={EditRecord} />
-                <Route path={MANAGE_RECORD} component={EditRecord} />
+                <Route path={`${MANAGE_RECORD}/:recordId`} component={ManageRecord} />
+                <Route path={MANAGE_RECORD} component={ManageRecord} />
                 <Route path={MANAGEMENT} component={Manager} />
                 <Route path={`${DASHBOARD}/:uid`} component={Dashboard} />
                 <Route path={DASHBOARD} component={Dashboard} />
